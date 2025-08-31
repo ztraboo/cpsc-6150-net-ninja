05b053d8d3dfc0e45d058db7e3bcfa5b1ee692fb
Zachary Trabookis <ztraboo@clemson.edu>
2025-08-31 13:23:41 -0400

    feat: Commiting the Net Ninja Quote Lab tutorial code that I wrote based on the YouTube videos.

---
+++ quotes_extended/android/app/src/main/kotlin/com/example/quotes/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.quotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ quotes_extended/lib/quote_dart.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+class QuoteCard extends StatelessWidget {
+  // const QuoteCard({
+  //   super.key,
+  // });
+
+  // Because of StatelessWidget we're saying that we're not changing this
+  // value once set. StatefulWidgets allow for change.
+  final Quote quote;
+
+  QuoteCard({ required this.quote });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.grey[600]
+              ),
+            ),
+            SizedBox(height: 6.0),
+            Text(
+              quote.author,
+              style: TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.grey[800]
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
